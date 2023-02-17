9b4266ad659398df7760e8e15df48b01c3ac5b10
pengsl-lab <67091321+pengsl-lab@users.noreply.github.com>
2023-02-17 20:53:08 +0800

    Delete split_data.sh

--- DataProcessing/split_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python create_dataset.py --input_file ../data/SSLdata/ClusterPre.txt
-python create_dataset.py --input_file ../data/SSLdata/PairDistance.txt
-python create_dataset.py --input_file ../data/SSLdata/PathClass.txt
-python create_dataset.py --input_file ../data/SSLdata/EdgeMask.txt
-python create_dataset.py --input_file ../data/SSLdata/SimReg.txt
-python create_dataset.py --input_file ../data/SSLdata/SimCon.txt
