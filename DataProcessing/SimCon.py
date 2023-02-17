857e470e4009c1fe084fd42b124e3a40fbbaa003
pengsl-lab <67091321+pengsl-lab@users.noreply.github.com>
2023-02-17 20:51:12 +0800

    Update SimCon.py

--- DataProcessing/SimCon.py
+++ DataProcessing/SimCon.py
@@ -11,7 +11,7 @@ drug_sim = np.loadtxt(path +"BioHNsdata/drug_sim.txt")
 pro_sim = np.loadtxt(path +"BioHNsdata/protein_sim.txt")
 disease_sim = np.loadtxt(path +"BioHNsdata/disease_sim.txt")
 drug_num, pro_num, disease_num = drug_sim.shape[0], pro_sim.shape[0], disease_sim.shape[0]
-total_num = drug_num + pro_num + disease_num
+total_num = drug_num + pro_num + disease_num 
 
 
 def sim_contrast(pairnum):
