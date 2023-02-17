955d1d3db43a45bb0a26402aa005ad12d9807b04
pengsl-lab <67091321+pengsl-lab@users.noreply.github.com>
2023-02-17 20:52:56 +0800

    Delete generating_SSL.sh

--- DataProcessing/generating_SSL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python degree_code.py --downstream DTI   --scenario warm
-python PairDistance.py  --downstream DTI   --scenario warm
-python SimCon.py  --downstream DTI   --scenario warm
-python EdgeMask.py  --downstream DTI   --scenario warm
-python PathClass.py  --downstream DTI   --scenario warm
-python SimReg.py  --downstream DTI   --scenario warm
-python ClusterPre.py   --downstream DTI   --scenario warm
